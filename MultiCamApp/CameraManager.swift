76f28e16baf34599469170365c5b5aa604af80e8
dev491999 <devashishgpatil@gmail.com>
2026-04-06 16:52:26 +0530

    Update: Implement manual rotation and 16:9 cropping for Ultra-Wide feed

--- MultiCamApp/CameraManager.swift
+++ MultiCamApp/CameraManager.swift
@@ -1,8 +1,9 @@
 import AVFoundation
 import UIKit
 import Photos
+import CoreImage
 
-class CameraManager: NSObject, ObservableObject, AVCaptureFileOutputRecordingDelegate {
+class CameraManager: NSObject, ObservableObject, AVCaptureFileOutputRecordingDelegate, AVCaptureVideoDataOutputSampleBufferDelegate {
     @Published var isRecording = false
     @Published var errorMessage: String?
     @Published var freeSpaceWarning = false
@@ -13,11 +14,21 @@ class CameraManager: NSObject, ObservableObject, AVCaptureFileOutputRecordingDel
     private var ultraWideDeviceInput: AVCaptureDeviceInput?
     private var audioInput: AVCaptureDeviceInput?
     
+    // Wide uses Simple Output (4K Vertical)
     private let wideOutput = AVCaptureMovieFileOutput()
-    private let ultraWideOutput = AVCaptureMovieFileOutput()
+    
+    // UltraWide uses Data Output for custom Rotate + Crop (1080p Horizontal)
+    private let ultraWideDataOutput = AVCaptureVideoDataOutput()
+    private var assetWriter: AVAssetWriter?
+    private var assetWriterInput: AVAssetWriterInput?
+    private var assetWriterPixelBufferAdaptor: AVAssetWriterInputPixelBufferAdaptor?
     
     private var wideFileURL: URL?
     private var ultraWideFileURL: URL?
+    
+    private let context = CIContext()
+    private var isAssetWriterReady = false
+    private let recordingQueue = DispatchQueue(label: "com.multicam.recording", qos: .userInitiated)
     
     override init() {
         super.init()
@@ -90,12 +101,15 @@ class CameraManager: NSObject, ObservableObject, AVCaptureFileOutputRecordingDel
             session.addInputWithNoConnections(audioInput)
         }
         
-        // 4. Outputs
+        // 4. Wide Output (MovieFileOutput)
         if session.canAddOutput(wideOutput) {
             session.addOutputWithNoConnections(wideOutput)
         }
-        if session.canAddOutput(ultraWideOutput) {
-            session.addOutputWithNoConnections(ultraWideOutput)
+        
+        // 5. UltraWide Output (VideoDataOutput)
+        if session.canAddOutput(ultraWideDataOutput) {
+            ultraWideDataOutput.setSampleBufferDelegate(self, queue: recordingQueue)
+            session.addOutputWithNoConnections(ultraWideDataOutput)
         }
         
         // Connections
@@ -107,9 +121,10 @@ class CameraManager: NSObject, ObservableObject, AVCaptureFileOutputRecordingDel
         }
         
         if let ultraWideVideoPort = ultraWideDeviceInput?.ports.first(where: { $0.mediaType == .video }),
-           session.canAddConnection(AVCaptureConnection(inputPorts: [ultraWideVideoPort], output: ultraWideOutput)) {
-            let connection = AVCaptureConnection(inputPorts: [ultraWideVideoPort], output: ultraWideOutput)
-            connection.videoOrientation = .landscapeRight
+           session.canAddConnection(AVCaptureConnection(inputPorts: [ultraWideVideoPort], output: ultraWideDataOutput)) {
+            let connection = AVCaptureConnection(inputPorts: [ultraWideVideoPort], output: ultraWideDataOutput)
+            // Keep buffer orientation as-is, we will rotate manually in delegate
+            connection.videoOrientation = .portrait
             session.addConnection(connection)
         }
         
@@ -140,24 +155,67 @@ class CameraManager: NSObject, ObservableObject, AVCaptureFileOutputRecordingDel
         }
         
         wideFileURL = generateFileName(type: "wide_4k")
-        ultraWideFileURL = generateFileName(type: "ultrawide_1080")
+        ultraWideFileURL = generateFileName(type: "ultrawide_1080_cropped")
+        
+        setupAssetWriter(url: ultraWideFileURL!)
         
         wideOutput.startRecording(to: wideFileURL!, recordingDelegate: self)
-        ultraWideOutput.startRecording(to: ultraWideFileURL!, recordingDelegate: self)
         
         DispatchQueue.main.async {
             self.isRecording = true
         }
     }
     
-    func stopRecording() {
-        wideOutput.stopRecording()
-        ultraWideOutput.stopRecording()
-        
-        DispatchQueue.main.async {
-            self.isRecording = false
+    func setupAssetWriter(url: URL) {
+        do {
+            assetWriter = try AVAssetWriter(outputURL: url, fileType: .mp4)
+            
+            let settings: [String: Any] = [
+                AVVideoCodecKey: AVVideoCodecType.h264,
+                AVVideoWidthKey: 1920,
+                AVVideoHeightKey: 1080,
+                AVVideoScalingModeKey: AVVideoScalingModeResizeAspectFill
+            ]
+            
+            assetWriterInput = AVAssetWriterInput(mediaType: .video, outputSettings: settings)
+            assetWriterInput?.expectsMediaDataInRealTime = true
+            
+            let attributes: [String: Any] = [
+                kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA,
+                kCVPixelBufferWidthKey as String: 1920,
+                kCVPixelBufferHeightKey as String: 1080
+            ]
+            
+            assetWriterPixelBufferAdaptor = AVAssetWriterInputPixelBufferAdaptor(
+                assetWriterInput: assetWriterInput!,
+                sourcePixelBufferAttributes: attributes
+            )
+            
+            if assetWriter!.canAdd(assetWriterInput!) {
+                assetWriter!.add(assetWriterInput!)
+            }
+            
+            isAssetWriterReady = false
+        } catch {
+            print("Failed to setup AssetWriter: \(error)")
         }
     }
+    
+    func stopRecording() {
+        wideOutput.stopRecording()
+        
+        recordingQueue.async {
+            self.isRecording = false
+            if self.assetWriter?.status == .writing {
+                self.assetWriterInput?.markAsFinished()
+                self.assetWriter?.finishWriting {
+                    StorageManager.saveVideo(url: self.ultraWideFileURL!)
+                }
+            }
+        }
+    }
+    
+    // MARK: - Delegates
     
     func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
         if let error = error {
@@ -165,5 +223,51 @@ class CameraManager: NSObject, ObservableObject, AVCaptureFileOutputRecordingDel
             return
         }
         StorageManager.saveVideo(url: outputFileURL)
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard isRecording, let assetWriter = assetWriter else { return }
+        
+        if !isAssetWriterReady {
+            assetWriter.startWriting()
+            assetWriter.startSession(atSourceTime: CMSampleBufferGetPresentationTimeStamp(sampleBuffer))
+            isAssetWriterReady = true
+        }
+        
+        guard let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer),
+              assetWriterInput?.isReadyForMoreMediaData == true else { return }
+        
+        // Process Frame: Rotate + Crop
+        let ciImage = CIImage(cvImageBuffer: imageBuffer)
+        
+        // 1. Rotate 90 degrees (Portrait -> Landscape)
+        // If device is vertical, we rotate right to make it a horizontal feed
+        let rotated = ciImage.oriented(.right)
+        
+        // 2. Crop to 16:9
+        // Original rotated bounds
+        let fullWidth = rotated.extent.width
+        let fullHeight = rotated.extent.height
+        
+        let targetWidth = fullWidth
+        let targetHeight = fullWidth * (9.0 / 16.0)
+        let yOffset = (fullHeight - targetHeight) / 2.0
+        
+        let cropRect = CGRect(x: 0, y: yOffset, width: targetWidth, height: targetHeight)
+        let cropped = rotated.cropped(to: cropRect)
+        
+        // 3. Scale to 1080p
+        let scaleX = 1920.0 / cropped.extent.width
+        let scaleY = 1080.0 / cropped.extent.height
+        let scaled = cropped.transformed(by: CGAffineTransform(scaleX: scaleX, y: scaleY))
+        
+        // Render to Pixel Buffer
+        var pixelBuffer: CVPixelBuffer?
+        CVPixelBufferPoolCreatePixelBuffer(nil, assetWriterPixelBufferAdaptor!.pixelBufferPool!, &pixelBuffer)
+        
+        if let pb = pixelBuffer {
+            context.render(scaled, to: pb)
+            assetWriterPixelBufferAdaptor?.append(pb, withPresentationTime: CMSampleBufferGetPresentationTimeStamp(sampleBuffer))
+        }
     }
 }
